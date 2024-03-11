51fbf3619813e63bed4870f0f65d32755c3d34d9
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-03-11 08:02:13 +0000

    Add isActive state and fix cursor styles in TimeLineBar

--- src/components/ui-elements/timeline.tsx
+++ src/components/ui-elements/timeline.tsx
@@ -153,13 +153,19 @@ function TimeLineMain({
 }
 function TimeLineBar({ zoomSize }: { zoomSize: number }) {
   const [currentFrame, setCurrentFrame] = useState(0);
+  const [isActive, setIsActive] = useState(false);
   return (
     <div
-      className="absolute top-0 w-0.5 h-full bg-gray-500 z-10 cursor-ew-resize opacity-50"
+      className={
+        (isActive && "active") +
+        " absolute top-0 w-0.5 h-full bg-gray-500 z-10 opacity-50 [&.active]:opacity-100 cursor-col-resize"
+      }
       style={{ left: `calc(5rem + ${currentFrame / zoomSize}px)` }}
       onMouseDown={e => {
         const startX = e.clientX;
         const startFrame = currentFrame;
+        setIsActive(true);
+        document.body.style.cursor = "col-resize";
         const onMouseMove = (e: MouseEvent) => {
           setCurrentFrame(
             Math.max(0, startFrame + (e.clientX - startX) * zoomSize)
@@ -168,6 +174,8 @@ function TimeLineBar({ zoomSize }: { zoomSize: number }) {
         const onMouseUp = () => {
           window.removeEventListener("mousemove", onMouseMove);
           window.removeEventListener("mouseup", onMouseUp);
+          setIsActive(false);
+          document.body.style.cursor = "auto";
         };
         window.addEventListener("mousemove", onMouseMove);
         window.addEventListener("mouseup", onMouseUp);
