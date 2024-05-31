b42f6c1b8a4fcce7e02621479990e7adfe4a416c
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-05-31 04:04:02 +0000

    create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Header from "@/components/element/header";
 import React from "react";
 export default function Page() {
   return (
-    <header>
-      <p>Clip Alchemist</p>
-    </header>
+    <>
+      <Header/>
+    </>
   );
 }
---
+++ src/components/element/header.tsx
@@ -0,0 +1,86 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import React from "react";
+export default function Header() {
+  const menubar = [
+    {
+      label: "File",
+      content: [
+        { label: "New" },
+        {
+          label: "Open",
+          content: [{ label: "Open file" }, { label: "Open folder" }],
+        },
+        { label: "Save", shortcut: "Ctrl+S" },
+        { label: "Save As", shortcut: "Ctrl+Shift+S" },
+        { label: "Close", shortcut: "Alt+F4" },
+      ],
+    },
+    {
+      label: "Edit",
+      content: [{ label: "Project Settings" }],
+    },
+    {
+      label: "Settings",
+      content: [{ label: "Manage extensions" }],
+    },
+    {
+      label: "Help",
+      content: [
+        { label: "About" },
+        { label: "Documentation", shortcut: "F1" },
+        { label: "Version Info" },
+      ],
+    },
+  ];
+  return (
+    <header>
+      <Menubar className="rounded-none border-0">
+        {menubar.map((menu) => (
+          <MenubarMenu>
+            <MenubarTrigger>{menu.label}</MenubarTrigger>
+            <MenubarContent>
+              {menu.content.map((item) =>
+                item.content ? (
+                  <Sub key={item.label} menu={item} />
+                ) : (
+                  <MenubarItem key={item.label}>
+                    {item.label}
+                    {item?.shortcut && (
+                      <MenubarShortcut>{item.shortcut}</MenubarShortcut>
+                    )}
+                  </MenubarItem>
+                ),
+              )}
+            </MenubarContent>
+          </MenubarMenu>
+        ))}
+      </Menubar>
+    </header>
+  );
+}
+function Sub({ menu }: { menu: any }) {
+  return (
+    <MenubarSub>
+      <MenubarSubTrigger>{menu.label}</MenubarSubTrigger>
+      <MenubarSubContent>
+        {menu.content.map((item: any) =>
+          item.content ? (
+            <Sub key={item.label} menu={item} />
+          ) : (
+            <MenubarItem key={item.label}>{item.label}</MenubarItem>
+          ),
+        )}
+      </MenubarSubContent>
+    </MenubarSub>
+  );
+}
