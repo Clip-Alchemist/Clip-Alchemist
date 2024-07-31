1b27d0f21d645d674692e72506fcc76620a0c1a4
takashi kajihara <142304219+kajitaka1201@users.noreply.github.com>
2024-07-31 07:00:55 +0000

    Timeline static basic UI completed

--- src/components/element/timeline/index.tsx
+++ src/components/element/timeline/index.tsx
@@ -11,12 +11,12 @@ import { cn } from "@/lib/utils";
 import { File } from "@/types/file";
 import { useState } from "react";
 export default function TileLine({ timelineData }: { timelineData?: File }) {
-  const [zoom, setZoom] = useState(1);
+  const [SliderValue, setSliderValue] = useState([50]);
   return (
     <div className="hidden-scrollbar h-full w-full overflow-scroll">
       <div className="w-max">
-        <div className="sticky inset-x-0 top-0 z-10 flex bg-gray-50">
-          <div className="sticky left-0 z-20 w-24 flex-none bg-gray-50">
+        <div className="sticky inset-x-0 top-0 z-10 flex border-b bg-gray-50 py-2">
+          <div className="sticky left-0 z-20 flex w-24 flex-none flex-col gap-1 bg-gray-50 px-2">
             <Select defaultValue="0">
               <SelectTrigger>
                 <SelectValue />
@@ -28,14 +28,35 @@ export default function TileLine({ timelineData }: { timelineData?: File }) {
                 </SelectGroup>
               </SelectContent>
             </Select>
-            <Slider defaultValue={[50]} max={100} />
+            <div
+              className="h-6 cursor-pointer rounded bg-blue-900"
+              onClick={(e) => {
+                const rect = e.currentTarget.getBoundingClientRect();
+                const x = e.clientX - rect.left;
+                setSliderValue([Math.round((x / rect.width) * 100)]);
+              }}
+            >
+              <div
+                className="h-full rounded-l bg-blue-400"
+                style={{ width: `${SliderValue}%` }}
+              ></div>
+              <Slider
+                defaultValue={[50]}
+                max={100}
+                value={SliderValue}
+                onValueChange={(value) => {
+                  setSliderValue(value);
+                }}
+                className="absolute left-0 top-0 h-full w-full cursor-pointer opacity-0"
+              />
+            </div>
           </div>
           <div className="flex flex-1">
             {new Array(100).fill(0).map((_, i) => (
               <div
                 key={i}
                 className={cn(
-                  "relative flex w-20 flex-none text-center",
+                  "relative flex w-40 flex-none text-center",
                   "before:absolute before:bottom-0 before:block before:h-1/2 before:w-px before:translate-x-[-0.5px] before:bg-black",
                 )}
               >
@@ -48,7 +69,7 @@ export default function TileLine({ timelineData }: { timelineData?: File }) {
                   <div
                     className={cn(
                       "absolute bottom-0 h-1/4 w-px bg-black",
-                      `left-${(index + 1) * 2}`,
+                      `left-${(index + 1) * 4}`,
                       "translate-x-[-0.5px]",
                     )}
                   />
@@ -57,7 +78,7 @@ export default function TileLine({ timelineData }: { timelineData?: File }) {
             ))}
           </div>
         </div>
-        <div className="left-10 left-12 left-14 left-16 left-18 left-2 left-20 left-4 left-6 left-8 hidden" />
+        <div className="left-12 left-16 left-20 left-24 left-28 left-32 left-36 left-4 left-8 hidden" />
         {/* The above code is required for compiling tailwind css */}
         <div>
           {new Array(25).fill(0).map((_, i) => (
